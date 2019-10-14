4c7e8730e5df8b37d77ede8da24ca36434830302
Dharamvir Singh <dharamvir.16@gmail.com>
2019-10-15 01:07:23 +0530

    Initial Version of Depedncy Injection

---
+++ DepedencyInjection/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepedencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger;
+            string loggerType = "text";
+            switch (loggerType)
+            {
+                case "text":
+                    logger = new DatabaseLogger();
+                    break;
+                default:
+                    logger = new TextLogger();
+                    break;
+            }
+            LogManager logManager = new LogManager(logger);
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception ex)
+            {
+                logManager.LogWritter(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    class LogManager
+    {
+        private ILogger _logger;
+        public LogManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void LogWritter(string message)
+        {
+            _logger.Log(message);
+        }
+    }
+    class TextLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Log to a text file : " + message);
+        }
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Log to a database : " + message);
+        }
+    }
+}
